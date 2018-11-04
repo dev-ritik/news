fbe776313535a80224e7b250d27f642d54ddace9
dev-ritik <ritikkne@gmail.com>
2018-11-04 11:24:29 +0530

    initial form linkage done

--- makeNews/forms.py
+++ makeNews/forms.py
@@ -6,14 +6,21 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class NewNewsForm(forms.Form):
-    news_time = forms.DateTimeField(help_text="Enter a date between now and 4 weeks (default 3).")
+    # news_time = forms.DateTimeField(help_text="Enter a date before today.")
+    news_content = forms.CharField(widget=forms.Textarea, help_text='Enter news content here')
+    headline = forms.CharField(max_length=200, help_text='Enter a headline')
+    highlight1 = forms.CharField(max_length=200, help_text='Enter highlight1')
+    highlight2 = forms.CharField(max_length=200, help_text='Enter highlight2')
+    link = forms.CharField(max_length=200, help_text='Enter a link')
+    print("NewNewsForm")
 
     def clean_news_time(self):
         data = self.cleaned_data['news_time']
-
+        # print("clean_news_time")
+        #
         # Check if a date is not in the past.
-        if data > datetime.now():
-            raise ValidationError(_('Invalid date - renewal in future'))
+        # if data > datetime.now():
+        #     raise ValidationError(_('Invalid date - renewal in future'))
 
         # Remember to always return the cleaned data.
         return data
--- makeNews/views.py
+++ makeNews/views.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 
-from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
-from django.urls import reverse
+from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponseRedirect
 
@@ -13,36 +10,47 @@ from makeNews.models import FakeNews
 
 def index(request):
     # If this is a POST request then process the Form data
-    book_instance = get_object_or_404(FakeNews)
-    if request.method == 'POST':
+    # book_instance = get_object_or_404(FakeNews)
+    newNews = FakeNews()
+    print("index")
+    # print(book_instance)
 
+    if request.method == 'POST':
+        print("post")
         # Create a form instance and populate it with data from the request (binding):
         new_news_form = NewNewsForm(request.POST)
-
+        print(new_news_form.errors)
+        print(new_news_form.error_class)
+        print(new_news_form.is_valid())
         # Check if the form is valid:
         if new_news_form.is_valid():
             # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            # book_instance.due_back = new_news_form.cleaned_data['renewal_date']
-            # book_instance.save()
+            newNews = FakeNews()
+            newNews.highlight1 = new_news_form.cleaned_data['highlight1']
+            newNews.highlight2 = new_news_form.cleaned_data['highlight2']
+            newNews.link = new_news_form.cleaned_data['link']
+            newNews.newsContent = new_news_form.cleaned_data['news_content']
+            newNews.headline = new_news_form.cleaned_data['headline']
+            newNews.date = datetime.now()
+            newNews.save()
 
+            urlcode = new_news_form.cleaned_data.get('highlight2')
             print("valid!!!")
             # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed'))
+            return HttpResponseRedirect('/makeNews/toi/' + urlcode)
 
     # If this is a GET (or any other method) create the default form.
     else:
+        print("get")
         time_now = datetime.now()
         new_news_form = NewNewsForm(initial={'renewal_date': time_now})
 
     context = {
         'form': new_news_form,
-        'book_instance': book_instance,
+        'fake_news': newNews,
     }
-
+    print("return")
     return render(request, 'new_news_form.html', context)
-
-
-    # return render(request, 'index.html', context={'fakeNews': 1})
 
 
 def error(request):
