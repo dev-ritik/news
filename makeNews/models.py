aff616ab5c13c8c900779911eea5cd97a74c5c82
dev-ritik <ritikkne@gmail.com>
2018-11-04 14:08:06 +0530

    added additional attributes

--- makeNews/models.py
+++ makeNews/models.py
@@ -8,10 +8,14 @@ class FakeNews(models.Model):
     """Model representing a fakenews."""
     newsContent = models.TextField(help_text='Enter news content here')
     headline = models.CharField(max_length=200, help_text='Enter a headline')
-    highlight1 = models.CharField(max_length=200, help_text='Enter highlight1', default=' ')
+    highlight1 = models.CharField(max_length=200, help_text='Enter highlight1')
     highlight2 = models.CharField(max_length=200, help_text='Enter highlight2', default=' ')
     date = models.DateTimeField()
+    place = models.CharField(max_length=80, help_text='Enter the place', default=' ')
+    author = models.CharField(max_length=50, help_text='Enter the author', default=' ')
     link = models.CharField(max_length=200, help_text='Enter a link', default=' ')
+    imageCaption = models.CharField(max_length=100, help_text='Enter a caption', default=' ')
+    imageUrl = models.URLField()
 
     class Meta:
         ordering = ['date']
@@ -24,4 +28,3 @@ class FakeNews(models.Model):
 
     def __str__(self):
         return str(self.headline)
-
